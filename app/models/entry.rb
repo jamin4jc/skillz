bad2b359f20bc10c195bb22f0988c0c4f9ebfd54
Benjamin Wagaman <ben@wagaman.org>
2017-03-14 18:01:25 -0400

    basic validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,9 @@
 class Entry < ApplicationRecord
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  def cool?
+    false
+  end
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  before {
-    @entry = Entry.new(name: 'Rspec', description: 'Testing Framework')
-  }
-  it { expect(@entry.name).to eql('Rspec') }
-  it { expect(@entry.description).to eql('Testing Framework') }
+  context "new entry with attributes" do
+    before { @entry = Entry.new(name: 'Rspec', description: 'Testing Framework')}
+
+    it { expect(@entry.name).to eql('Rspec') }
+    it { expect(@entry.description).to eql('Testing Framework') }
+    it { expect(@entry).to be_valid }
+  end
+
+
+  context "new entry without a name" do
+    before { @entry = Entry.new() }
+    describe "validations" do
+      it { expect(@entry).to_not be_valid }
+    end
+  end
+
+  context "new entry with a duplicate name" do
+    before {
+      @original_entry = Entry.create(name: 'JS')
+      @entry = Entry.new(name: 'JS')
+     }
+    describe "validations" do
+      it { expect(@entry).to_not be_valid }
+    end
+  end
 end
